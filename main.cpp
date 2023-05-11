d7f03589bc0f596911a0d3bcc44e77321c48583b
Lényi Martin <lenyim00@gmail.com>
2023-05-11 22:49:05 +0200

    Add argument check and some usage message

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void printUsage() {
+    std::cout << "usage:" << std::endl;
+    std::cout << "\t./parser -caff [path-to-caff].caff" << std::endl;
+    std::cout << "\t./parser -ciff [path-to-ciff].ciff" << std::endl;
+}
+
+bool checkOption(const std::string& optionName) {
+    if (optionName != "-caff" && optionName != "-ciff") {
+        printUsage();
+        return false;
+    }
+    return true;
+}
+
+bool checkFileName(const std::string& optionName, const std::string& fileName) {
+    auto selectedFileType = optionName.substr(1);
+
+    auto dotPosition = fileName.find_last_of('.');
+    auto fileExtension = fileName.substr(dotPosition + 1);
+
+    if (selectedFileType != fileExtension) {
+        std::cout << "The selected file type '" << fileExtension << "' is not valid for the selected option '" << optionName << "'." << std::endl;
+        printUsage();
+        return false;
+    }
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    auto argumentCount = argc - 1;
+    if (argumentCount != 2) {
+        printUsage();
+        return -1;
+    }
+
+    std::string optionName = argv[1];
+    std::string fileName = argv[2];
+
+    if (!checkOption(optionName) || !checkFileName(optionName, fileName)) {
+        return -1;
+    }
+
     return 0;
 }
